e96f48335e841efedbe352f3104c74884844ebf5
John Lombardo <salvatore.lombardo@disney.com>
2023-12-28 11:56:40 -0800

    Use a template: https://docs.djangoproject.com/en/4.2/intro/tutorial03/#write-views-that-actually-do-something

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    return render(
+        request,
+        "polls/index.html",
+        {
+            "latest_question_list": latest_question_list
+        }
+    )
 
 
 def detail(request, question_id):
